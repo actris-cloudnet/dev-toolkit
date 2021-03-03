1d25e5c601ba8aa2c858bcb4ab1560ee0b60a450
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2021-03-03 11:39:16 +0200

    Fetch also SS database

--- populate-db.sh
+++ populate-db.sh
@@ -5,18 +5,33 @@ export PGPORT=54321
 export PGUSER=dataportal
 export PGPASSWORD=dev
 
-cachefile="remote-db.tmp.sql"
+cachefile_dp="tmp/dataportal-db.sql"
+cachefile_ss="tmp/ss-db.sql"
 
-if test "$1" == '-u' -o -e $cachefile; then
-  if [ -z "$DATAPORTAL_SSH" ]; then
-    echo "Environment variable DATAPORTAL_SSH must be set, i.e user@host"
+if test "$1" == '-u' -o ! -e "$cachefile_db" -o ! -e "$cachefile_db"; then
+  if test -z "$DATAPORTAL_SSH" -o -z "$CLOUDNET_SSH"; then
+    echo "Environment variables DATAPORTAL_SSH and CLOUDNET_SSH must be set, i.e user@host"
     exit 1
   fi
+
+  mkdir -p tmp
 
   echo "Fetching remote db..."
   dropdb dataportal
   createdb dataportal
-  ssh -C "$DATAPORTAL_SSH" "pg_dump dataportal" | tee $cachefile | psql dataportal
+  ssh -C "$DATAPORTAL_SSH" "pg_dump dataportal" | tee $cachefile_dp | psql dataportal
+
+  export PGUSER=ss
+  dropdb ss
+  createdb ss
+  ssh -C "$CLOUDNET_SSH" "pg_dump ss" | tee $cachefile_ss | psql ss
 else
-  psql dataportal < $cachefile
+  dropdb dataportal
+  createdb dataportal
+  psql dataportal < $cachefile_dp
+
+  export PGUSER=ss
+  dropdb ss
+  createdb ss
+  psql ss < $cachefile_ss
 fi
