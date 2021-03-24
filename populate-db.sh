5181b1f878f1713a22e3374096c2f3aab6559a0f
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2021-03-24 11:44:32 +0200

    Refactor

--- populate-db.sh
+++ populate-db.sh
@@ -8,30 +8,39 @@ export PGPASSWORD=dev
 cachefile_dp="tmp/dataportal-db.sql"
 cachefile_ss="tmp/ss-db.sql"
 
-if test "$1" == '-u' -o ! -e "$cachefile_db" -o ! -e "$cachefile_db"; then
+function resetdb {
+  dropdb $1
+  createdb $1
+}
+
+docker-compose up -d db
+echo -n "Waiting for db... "
+until psql -c "select 1" > /dev/null 2>&1
+do
+  sleep 1
+done
+echo "OK"
+
+if test "$1" == '-u' -o ! -e "$cachefile_dp" -o ! -e "$cachefile_ss"; then
+  echo "Fetching remote db..."
   if test -z "$DATAPORTAL_SSH" -o -z "$CLOUDNET_SSH"; then
     echo "Environment variables DATAPORTAL_SSH and CLOUDNET_SSH must be set, i.e user@host"
     exit 1
   fi
 
-  mkdir -p tmp
-
-  echo "Fetching remote db..."
-  dropdb dataportal
-  createdb dataportal
+  resetdb dataportal
   ssh -C "$DATAPORTAL_SSH" "pg_dump dataportal" | tee $cachefile_dp | psql dataportal
 
   export PGUSER=ss
-  dropdb ss
-  createdb ss
+  resetdb ss
   ssh -C "$CLOUDNET_SSH" "pg_dump ss" | tee $cachefile_ss | psql ss
 else
-  dropdb dataportal
-  createdb dataportal
+  echo "Using cached db..."
+
+  resetdb dataportal
   psql dataportal < $cachefile_dp
 
   export PGUSER=ss
-  dropdb ss
-  createdb ss
+  resetdb ss
   psql ss < $cachefile_ss
 fi
