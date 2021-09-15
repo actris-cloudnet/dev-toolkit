b91ab0117ccc8af4f6eb4c88ab184401fa4a8005
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2021-09-15 14:35:28 +0300

    Use secrets

--- start.sh
+++ start.sh
@@ -4,13 +4,12 @@ set -e
 mode=$1
 
 if test "$mode" == 'remote'; then
-  set -o xtrace
-  cd ../storage-service
+  cd ../secrets
   git-crypt unlock
+  export $(cat private/s3-ro.env | xargs)
   cd -
   SS_MODE=remote docker-compose up
 elif test "$mode" == 'local'; then
-  set -o xtrace
   SS_MODE=local docker-compose up
 else
   echo "Usage: $0 remote|local"
