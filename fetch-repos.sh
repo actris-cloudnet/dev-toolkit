75633fa3dc9988752404af499840e60cefb2e7d4
Tuomas Siipola <tuomas.siipola@fmi.fi>
2022-03-22 09:57:41 +0200

    Make output clearer

--- fetch-repos.sh
+++ fetch-repos.sh
@@ -5,17 +5,18 @@ function fetch-repo {
     echo "Pulling '$1'"
     if ! git -C "../$1" pull --ff-only; then
       echo "ERROR: Conflicting changes detected. Please fix this manually."
-      return
-    fi
-    current_branch="$(git -C "../$1" rev-parse --abbrev-ref HEAD)"
-    default_branch="$(git -C "../$1" remote show origin | sed -n '/HEAD branch/s/.*: //p')"
-    if [ "$current_branch" != "$default_branch" ]; then
-      echo "WARN: Current branch '$current_branch' is not the default branch '$default_branch'"
+    else
+      current_branch="$(git -C "../$1" rev-parse --abbrev-ref HEAD)"
+      default_branch="$(git -C "../$1" remote show origin | sed -n '/HEAD branch/s/.*: //p')"
+      if [ "$current_branch" != "$default_branch" ]; then
+        echo "WARN: Current branch '$current_branch' is not the default branch '$default_branch'"
+      fi
     fi
   elif [ "$2" = true ]; then
     echo "Cloning '$1'"
     git -C .. clone "git@github.com:actris-cloudnet/$1.git"
   fi
+  echo
 }
 
 # Required repositories.
